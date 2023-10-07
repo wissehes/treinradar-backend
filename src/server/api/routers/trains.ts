086bcc476c48903d396170aa15552efe7022d9d4
Wisse <wissehes@gmail.com>
2023-10-07 17:14:42 +0200

    Add method for all live trains

--- src/server/api/routers/trains.ts
+++ src/server/api/routers/trains.ts
@@ -4,6 +4,14 @@ import getTrains from "~/queries/getTrains";
 import { getTrainInfo } from "~/queries/getTrainsInfo";
 import { createTRPCRouter, publicProcedure } from "~/server/api/trpc";
 import { type LiveTrain, liveTrainSchema } from "~/types/LiveTrain";
+import { type TrainInformation } from "~/types/NS/getTrainInfoResponse";
+
+const createImageURL = (info?: TrainInformation): string | undefined => {
+  const type = info?.materieeldelen[0]?.type;
+  if (!type) return;
+
+  return `https://trein.wissehes.nl/api/image/${encodeURIComponent(type)}`;
+};
 
 export const trainsRouter = createTRPCRouter({
   live: publicProcedure
@@ -34,10 +42,49 @@ export const trainsRouter = createTRPCRouter({
 
         speed: thisTrain.snelheid,
         direction: thisTrain.richting,
-        track: info?.spoor,
 
-        image: `https://trein.wissehes.nl/api/image/${info?.materieeldelen[0]?.type}`,
+        station: info?.station,
+        track: info?.spoor,
+        platformFacilities: info?.perronVoorzieningen,
+
+        image: createImageURL(info),
       };
+
+      return data;
+    }),
+
+  all: publicProcedure
+    .meta({
+      openapi: {
+        method: "GET",
+        path: "/trains/live/all",
+        description: "Get all live trains",
+        tags: ["trains"],
+      },
+    })
+    .input(z.void())
+    .output(z.array(liveTrainSchema))
+    .query(async () => {
+      const allTrains = await getTrains();
+      const infos = await getTrainInfo(allTrains);
+
+      const data: LiveTrain[] = allTrains.map((train) => {
+        const info = infos.find((i) => i.ritnummer == train.treinNummer);
+        // info.
+        return {
+          latitude: train.lat,
+          longitude: train.lng,
+
+          speed: train.snelheid,
+          direction: train.richting,
+
+          station: info?.station,
+          track: info?.spoor,
+          platformFacilities: info?.perronVoorzieningen,
+
+          image: createImageURL(info),
+        };
+      });
 
       return data;
     }),
